ec0fae92b613fee55d32e11086a175a18e4899aa
MariaEdPaixao <mariaeduardaalvesdapaixao0807@gmail.com>
2024-09-13 22:13:15 -0300

    Escondendo o Footer e Header
    quando estiverem na página login

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
-import { Outlet } from "react-router-dom"
-import Cabecalho from "./components/Cabecalho"
-import Rodape from "./components/Rodape"
-
+import { Outlet, useLocation } from "react-router-dom";
+import Cabecalho from "./components/Cabecalho";
+import Rodape from "./components/Rodape";
 
 function App() {
+  const location = useLocation();
+
+  // Rotas onde o Cabecalho e Rodape devem ser escondidos
+  const hiddenHeaderFooterRoutes = ["/", "/login"];
+
+  // Verificando se a rota atual está em hiddenHeaderFooterRoutes
+  const shouldHideHeaderFooter = hiddenHeaderFooterRoutes.includes(location.pathname);
 
   return (
     <>
-      <Cabecalho/>
-      <Outlet/>
-     <Rodape/>
+      {/* Renderizando o Cabecalho apenas se a rota não estiver em hiddenHeaderFooterRoutes */}
+      {!shouldHideHeaderFooter && <Cabecalho />}
+
+      <Outlet />
+
+      {/* Renderizando o Rodape apenas se a rota não estiver em hiddenHeaderFooterRoutes */}
+      {!shouldHideHeaderFooter && <Rodape />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
